b0142669ca65ae152511d093b70368e1a03b54bb
Arnav-Sharma-3 <arnavsharma110305@gmail.com>
2026-06-03 03:40:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,11 +117,10 @@ def compute_fields_lobes(alpha, g1, g2, v0, s_v0, z, t_age, geometry,
     B_eq = (2/(1+alpha))**(1/(3+alpha))*B_min
 
     u_B = B_eq**2/(8*math.pi)
-    #u_p = alpha*A*L*B_eq**(-1.5)/V_cm3
     u_p = (A*(B_eq**(-(1+alpha))))/V_cm3
     u_tot = u_p + u_B
 
-    U_eq = 2 * u_B * V_cm3
+    U_eq = (u_p+u_B)*V_cm3
     P_jet_W = (U_eq / (t_age*3.15576e7)) * 1e-7
 
     return dict(
